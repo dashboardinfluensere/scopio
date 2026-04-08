dc2349acc144a9777d583ba9a0abefaa226b3cff
Dmytro Maliarchuk <dashboard.influensere@gmail.com>
2026-04-08 20:23:11 +0200

    fix: clerk middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,16 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
-export default clerkMiddleware(async (_auth, req) => {
-  const requestHeaders = new Headers(req.headers);
-  requestHeaders.set("x-current-path", req.nextUrl.pathname);
-
-  return NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!_next|.*\\..*).*)"],
